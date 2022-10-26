fc90e688cc3c0429d2ddb0512f90392bdba3d6c3
Francis-Frimpong <brakay1136@gmail.com>
2022-10-26 11:18:09 +0000

    additional code for css and javascript

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const formField = document.querySelector(".form-field");
 const inputField = document.querySelector("#input-field");
 const evenNumBtn = document.querySelector(".even-num-btn");
 const oddNumBtn = document.querySelector(".odd-num-btn");
-const clearBtn = document.querySelector("clear-field-btn");
+const clearBtn = document.querySelector(".clear-field-btn");
 const  results= document.querySelector(".results");
+
 
 
 
@@ -11,15 +12,21 @@ const  results= document.querySelector(".results");
 evenNumBtn.addEventListener('click', (e)=>{
     e.preventDefault("formField");
     findEvenNumbers();
-        
+    clear()
+    
+    
 })
 
 oddNumBtn.addEventListener('click', (e)=>{
     e.preventDefault("formField");
     findOddNumbers();
-
+    clear()
+    
 })
 
+
+
+let clear = () => formField.reset();
 
 
 //function to find the even numbers
@@ -27,7 +34,8 @@ let findEvenNumbers = () => {
     let strNum = inputField.value;
     let splitStr = Array.from(String(strNum),Number);
     let getEvenNumbers = splitStr.filter(number => number % 2 === 0);
-    results.textContent = ` The even numbers are: ${getEvenNumbers}`; 
+    results.textContent = ` The even numbers are: ${getEvenNumbers}`;
+     
             
 }
 
@@ -37,12 +45,13 @@ let findOddNumbers = () => {
     let splitStr = Array.from(String(strNum),Number);
     let getOddNumbers = splitStr.filter(number => number % 2 !== 0);
     results.textContent = ` The odd numbers are: ${getOddNumbers}`; 
+    
+
 }
 
-clearBtn.addEventListener('click', (e)=>{
-    e.preventDefault("formField")
-    inputField.reset();
-})
+
+
+
 
 
 
